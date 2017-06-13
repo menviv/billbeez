d92b7c163891b27206b3c56acbf6f9e7fdfd43fa
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-06-13 19:25:46 +0300

    version13

--- messages/index.js
+++ messages/index.js
@@ -44,7 +44,7 @@ bot.dialog('/', [
             if (msg.attachments && msg.attachments.length > 0) {
             // Echo back attachment
             var attachment = msg.attachments[0];
-            session.send("dddddd:", attachment.contentUrl);
+            session.send("dddddd:" + attachment.contentUrl);
                 session.send({
                     text: "You sent:",
                     attachments: [
